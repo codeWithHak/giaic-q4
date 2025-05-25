7cead94debf2ad040ca033051d6ee65e83a856fe
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2025-05-25 17:13:35 +0500

    see the response time from llm

---
+++ 04-hello-agent/main.py
@@ -0,0 +1,26 @@
+import chainlit as cl
+from datetime import datetime
+
+# real code
+@cl.on_message
+async def res(message:cl.Message):
+    try:
+        curr_time = datetime.utcnow()
+        created_at = datetime.fromisoformat(message.created_at.rstrip("z"))
+        if not created_at:
+            raise ValueError("message.create_at is missing")
+
+        total_time = f"{(curr_time - created_at).total_seconds():.3f}"
+        await cl.Message(content=f"Recieved {message.content} \n Responded in: {total_time} seconds").send()
+    except Exception as e:
+        await cl.Message(content=f"Error: {e}").send()
+        
+        
+
+# easy code
+
+# @cl.on_message
+# async def res(message:cl.Message):
+#     message = cl.Message(content=f"Hellooo {message.content}")
+#     await message.send()
+    # created_at
