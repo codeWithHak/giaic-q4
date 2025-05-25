e251325d43d2cbe3051eb6e50a4d0fe1f365ab4f
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2025-05-25 21:11:19 +0500

    complete one way call

--- 04-hello-agent/main.py
+++ 04-hello-agent/main.py
@@ -4,19 +4,24 @@ from datetime import datetime
 
 
 
-# real code
+# template code
 @cl.on_message
-async def res(message:cl.Message):
-    try:
-        curr_time = datetime.utcnow()
-        created_at = datetime.fromisoformat(message.created_at.rstrip("z"))
-        if not created_at:
-            raise ValueError("message.create_at is missing")
+async def res(message):
+    await cl.Message(f"Recieved {message.content} \n created at: {message.created_at}").send()
 
-        total_time = f"{(curr_time - created_at).total_seconds():.3f}"
-        await cl.Message(content=f"Recieved {message.content} \n Responded in: {total_time} seconds").send()
-    except Exception as e:
-        await cl.Message(content=f"Error: {e}").send()
+# my experiment
+# @cl.on_message
+# async def res(message:cl.Message):
+#     try:
+#         curr_time = datetime.utcnow()
+#         created_at = datetime.fromisoformat(message.created_at.rstrip("z"))
+#         if not created_at:
+#             raise ValueError("message.create_at is missing")
+
+#         total_time = f"{(curr_time - created_at).total_seconds():.3f}"
+#         await cl.Message(content=f"Recieved {message.content} \n Responded in: {total_time} seconds").send()
+#     except Exception as e:
+#         await cl.Message(content=f"Error: {e}").send()
         
         
 
