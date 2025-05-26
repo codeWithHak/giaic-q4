505bd35a706a9900e1396d5c5a657a127ed668af
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2025-05-26 17:31:57 +0500

    complete chatbot with history

--- 06-openai-agents-sdk/main.py
+++ 06-openai-agents-sdk/main.py
@@ -1,34 +1,55 @@
 import asyncio
 from openai import AsyncOpenAI # chat completions
-from agents import Agent, OpenAIChatCompletionsModel, Runner, set_tracing_disabled
+from agents import Agent, OpenAIChatCompletionsModel, Runner
 import os
 from dotenv import load_dotenv
+import chainlit as cl
+
 load_dotenv()
-set_tracing_disabled(disabled=True) # Open AI Tracing == Disable
+
+api_key = os.getenv("OPENROUTER_API_KEY")
+base_url = os.getenv("BASE_URL")
+url = os.getenv("URL")
+model = os.getenv("MODEL")
+
+
+
 print("set tracing passed")
 
 client = AsyncOpenAI(
-    api_key=os.getenv("OPENROUTER_API_KEY"),
-    base_url=os.getenv("BASE_URL")
+    api_key=api_key,
+    base_url=base_url
 )
 
 print("client set")
 
 
-async def main():
     # This agent will use the custom LLM provider
-    agent = Agent(
-        name="LahoreTA",
-        instructions="You only respond in english.",
-        model=OpenAIChatCompletionsModel(model=os.getenv("MODEL"), openai_client=client),
-    )
+agent = Agent(
+    name="Assistant",
+    instructions="You only respond in english.",
+    model=OpenAIChatCompletionsModel(model=model, openai_client=client),
+)
 
+print("before printing result")
+
+
+@cl.on_chat_start
+async def handle_chat_start():
+    cl.user_session.set("history",[])
+    await cl.Message(content="Hello how may I help you sirrr").send()
+
+@cl.on_message
+async def handle_message(message:cl.Message):
+    
+    history = cl.user_session.get("history")
+    
+    history.append({"role":"user", "content":message.content})
     result = await Runner.run(
         agent, # starting agent
-        "What is your name?.", # request
-    )
-    print("before printing result")
-    print(result.final_output)
-
-
-asyncio.run(main())
+        input=history
+)   
+    history.append({"role":"assistant", "content":result.final_output})
+    cl.user_session.set("history",history)
+    
+    await cl.Message(content=result.final_output).send() 
