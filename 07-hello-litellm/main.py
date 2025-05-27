1248fa65f1535733e73cc609dc2fcd3707bd40bb
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2025-05-27 13:35:11 +0500

    complete first agent using litellm

--- 07-hello-litellm/main.py
+++ 07-hello-litellm/main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from 07-hello-litellm!")
+from litellm import completion
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+## set ENV variables
+os.environ["BASE_URL"] = os.getenv("BASE_URL")
+os.environ["OPENROUTER_API_KEY"] = os.getenv("OPENROUTER_API_KEY")
+def call_mistral():
+    user_input = input("Ask me anything: ")
 
-if __name__ == "__main__":
-    main()
+    response = completion(
+      model="openrouter/mistralai/mistral-7b-instruct",
+      messages=[{ "content": user_input,"role": "user"}]
+    )
+
+    print(response['choices'][0]['message']['content'])
+    
+call_mistral()
