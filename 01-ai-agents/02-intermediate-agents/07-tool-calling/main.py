87cb6a3798dbaa99abd860b6dcc60652f619ca3b
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2025-06-23 18:53:46 +0500

    set up basic agent

--- 01-ai-agents/02-intermediate-agents/07-tool-calling/main.py
+++ 01-ai-agents/02-intermediate-agents/07-tool-calling/main.py
@@ -1,6 +1,31 @@
-def main():
-    print("Hello from 07-tool-calling!")
+from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel, set_tracing_disabled
+from dotenv import load_dotenv
+import asyncio
+import os
+
+load_dotenv()
+set_tracing_disabled(disabled=True)
 
 
-if __name__ == "__main__":
-    main()
+API_KEY = os.getenv("API_KEY")
+BASE_URL = os.getenv("BASE_URL")
+MODEL = os.getenv("MODEL")
+
+external_client = AsyncOpenAI(
+    api_key=API_KEY,
+    base_url=BASE_URL
+)
+
+agent = Agent(
+    name="Assistant",
+    instructions="You are a helpful assistant",
+    model=OpenAIChatCompletionsModel(model=MODEL, openai_client=external_client)
+)
+
+async def main():
+    result = await Runner.run(starting_agent=agent, input="Hello")
+    print(result.final_output)
+    
+asyncio.run(main())
+
+
