cb664f57f82515310181006bce984654f20aedb8
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2025-07-07 08:22:46 +0500

    setup basic agent

---
+++ 01-ai-agents/06-crash-course/01-class/main.py
@@ -0,0 +1,21 @@
+from agents import AsyncOpenAI, OpenAIChatCompletionsModel, Agent, Runner, set_tracing_disabled
+from dotenv import load_dotenv
+import os
+from rich import print
+
+set_tracing_disabled(disabled=True)
+load_dotenv()
+
+external_client = AsyncOpenAI(
+    api_key=os.getenv("API_KEY"),
+    base_url=os.getenv("BASE_URL")
+)
+
+agent = Agent(
+    name="assistant",
+    instructions="You are a helpful assistant",
+    model=OpenAIChatCompletionsModel(model="gemini-2.5-flash", openai_client=external_client)
+)
+
+result = Runner.run_sync(starting_agent=agent, input="Hello")
+print(result)
