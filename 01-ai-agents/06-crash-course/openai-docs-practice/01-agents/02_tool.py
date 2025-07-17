cce64ef57abcc26a3f472d18427909343e7ffb42
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2025-07-17 09:42:45 +0500

    add requests

--- 01-ai-agents/06-crash-course/openai-docs-practice/01-agents/02_tool.py
+++ 01-ai-agents/06-crash-course/openai-docs-practice/01-agents/02_tool.py
@@ -1,15 +1,30 @@
 from agents import Agent, Runner, enable_verbose_stdout_logging, set_default_openai_api, function_tool
 from dotenv import load_dotenv
 import os
+import requests
+
 
 enable_verbose_stdout_logging()
 set_default_openai_api("chat_completions")
 
 load_dotenv()
 
-@function_tool
-def get_weather():
-    return ""
+
+@function_tool()
+def get_weather(city:str) -> str:
+    
+    """Takes city as an argument
+    
+    Args(str):city
+  
+    returns weather of that city  
+    """
+    response = requests.get("")
+    
+    "Read Docs: https://www.weatherapi.com/docs/"
+    "WEATHER_API_KEY=a2dc1d4874ab437189543444251707"
+        
+    return f"The weather in {city} is sunny"
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
@@ -17,8 +32,8 @@ agent = Agent(
     name="Assistant",
     instructions="You are a helpful assistant",
     model="gpt-4o",
-
+    tools=[get_weather]
 )
 
-result = Runner.run_sync(starting_agent=agent, input="Hello which llm e.g(gemini) and model e.g(gemini-2.5-flash) is this")
+result = Runner.run_sync(starting_agent=agent, input="What's the weather in Karachi")
 print(result.final_output)
