f6dcc962eb9690f316da6199e384f55799f15d1b
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2025-07-20 15:31:23 +0500

    apply pydantic class for batsman stats

--- 01-ai-agents/06-crash-course/openai-docs-practice/01-agents/03_output_types.py
+++ 01-ai-agents/06-crash-course/openai-docs-practice/01-agents/03_output_types.py
@@ -2,14 +2,18 @@ from agents import Agent, Runner, AgentOutputSchema, enable_verbose_stdout_loggi
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from rich import print 
-import agents.result
+
 
 load_dotenv()
 enable_verbose_stdout_logging()
 set_default_openai_api("chat_completions")
 
-class JsonOutput(BaseModel):
-    result:dict[str,str]
+
+# !!! ----- 2 ------- !!! advanced type
+class BattingStats(BaseModel):
+    batsman_name: str
+    highest_score: int
+    average: float
 
 agent = Agent(
     name="Assistant",
@@ -19,11 +23,27 @@ agent = Agent(
     
     # output_type=int
     # output_type=str
-    # output_type=AgentOutputSchema(dict[str,str], strict_json_schema=False)
+    
+    # !!! ----- 2 ------- !!! advanced tpes
+    output_type=list[BattingStats]
 )
 
-result = Runner.run_sync(starting_agent=agent, input="What is 2+2")
+# result = Runner.run_sync(starting_agent=agent, input="What is 2+2")
 
-print(result)
+# !!! ----- 2 ------- !!!
+result = Runner.run_sync(starting_agent=agent, input="Ahmed Shehzad scored 99 runs in his last psl match and after that he scored 50 runs in 3 more matches, Babar Azam scored 3 consecutive centuroes against windies with the highest score of 120 and then scored 3 ducks against Australia ")
+
+
+# if we just print result.final_output like this,
+# print(result.final_output)
+
+# we will get a list of instances like: 
+#[
+#    BattingStats(batsman_name='Ahmed Shehzad', highest_score=99, average=62.75),
+#    BattingStats(batsman_name='Babar Azam', highest_score=120, average=60.0)
+#]
+
+# But we want list of dicts so we use .model_dump() for each instance and get result in dict. 
+print([res.model_dump() for res in result.final_output])
 print(type(result))
 
