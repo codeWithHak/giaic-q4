c2cde09c8e152e2962080967aa0621336dbbc206
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2025-08-11 20:46:18 +0500

    explore model settings

---
+++ 01-ai-agents/06-crash-course/02-class/07_model_settings.py
@@ -0,0 +1,21 @@
+from agents import Agent, Runner,enable_verbose_stdout_logging, ModelSettings
+from dotenv import load_dotenv
+import os
+from rich import print
+
+load_dotenv()
+
+enable_verbose_stdout_logging()
+
+agent = Agent(
+    name="assistant",
+    instructions="You are a helpful assistant",
+    model="gpt-4o-mini",
+    model_settings=ModelSettings(
+        
+    )
+)
+
+result = Runner.run_sync(starting_agent=agent, input="What is ai")
+
+print("result:",result.final_output)
